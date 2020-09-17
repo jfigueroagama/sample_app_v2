370448f77490ef443713ac421b291238226d57b2
Josué Gama <josue.figueroa@magmalabs.io>
2020-09-17 12:01:46 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # Some Rails shortcuts
   #  Full command     Shortcut
   # $ rails server    $ rails s
@@ -7,7 +8,8 @@ Rails.application.routes.draw do
   # $ rails test      $ rails t
   # $ bundle install  $ bundle
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
